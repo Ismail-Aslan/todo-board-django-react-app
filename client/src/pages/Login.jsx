aa7c2c8fa610873278884fc702ec3ac4e49b50b7
İsmail ASLAN <1aslan1ismail@gmail.com>
2022-03-05 18:31:56 +0300

    add login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import { Container, Row, Col, Form, Button, InputGroup } from "react-bootstrap";
+import { useNavigate,Link } from "react-router-dom";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const [showPass, setShowPass] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit");
+  };
   return (
-    <div>Login</div>
-  )
+    <Container className="bg-info bg-opacity-25 pt-5 min-vh-100" fluid>
+      <Row className="bg-light rounded w-75 mx-auto align-items-center shadow-lg">
+        <Col
+          className="rounded-start px-3 pt-5 pb-3 m-0"
+          lg="5"
+          md="6"
+          xs="12"
+        >
+          <Row className="text-dark text-center mb-4">
+            <h3>Welcome Back!</h3>
+            <h5>Login to ToDo Board</h5>
+          </Row>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label className="text-dark">Email:</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email"
+                ref={email}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please write your email address.
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label className="text-dark">Password:</Form.Label>
+              <InputGroup>
+                <Form.Control
+                  type={!showPass ? "password" : "text"}
+                  placeholder="Enter your password"
+                  ref={password}
+                  required
+                />
+                <InputGroup.Text>
+                  <i
+                    onClick={() => {
+                      setShowPass(!showPass);
+                    }}
+                    className={showPass ? "fas fa-eye-slash" : "fas fa-eye"}
+                  ></i>
+                </InputGroup.Text>
+              </InputGroup>
+            </Form.Group>
+            <Row className="text-center">
+              <Button className="w-50 mx-auto" variant="primary" type="submit">
+                Login
+              </Button>
+            </Row>
+          </Form>
+            
+            <p className="text-center mt-3">Don't you have an account? <Link to="/register">Register Here</Link></p>
+        </Col>
+        <Col
+        className="rounded-end p-0 m-0 d-none d-md-block"
+        lg="7"
+        md="6"
+        >
+          <img  className="rounded-end m-0" src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700" alt="Login" width="100%" height="100%" />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
