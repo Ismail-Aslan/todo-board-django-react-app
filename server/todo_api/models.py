d0871e12581d984407938306e89dd4c89beab4c8
İsmail ASLAN <1aslan1ismail@gmail.com>
2022-03-06 23:47:58 +0300

    add models

--- server/todo_api/models.py
+++ server/todo_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Task(models.Model):
+    id = models.TextField(blank=True,primary_key=True)
+    content = models.TextField(null=True,blank=True)
+    tags = ArrayField( models.TextField(null=True,blank=True))
+    assignedTo = models.TextField(null=True,blank=True)
+    color = models.TextField(null=True,blank=True)
+    column=models.ForeignKey(
+        'Column',
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return f"{self.id} {self.content}"
+
+class Column(models.Model):
+    id = models.TextField(blank=True,primary_key=True)
+    title = models.TextField(null=True,blank=True)
+    
+    def __str__(self):
+        return f"{self.id} {self.title}"
+    
+class Board(models.Model):
+    order= ArrayField( models.TextField(null=True,blank=True))
+    
+    def __str__(self):
+        return f"{self.order}"
+    
