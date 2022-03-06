df817b5db3e2a754dbf0f05520a75e1bc6cd8f7a
İsmail ASLAN <1aslan1ismail@gmail.com>
2022-03-06 23:36:47 +0300

    add restframework and todo api

--- server/main/settings.py
+++ server/main/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-01es3mp$r^8x3%ydkr5#@if7y82-)!5njwn(4rv0ho)ot=@h1c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # third party apps
+    'rest_framework',
+    
+    
+    # my apps
+    'todo_api',
 ]
 
 MIDDLEWARE = [
