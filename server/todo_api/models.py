6a1ca4963db5631d720a9eadb3d613e4df8569be
İsmail ASLAN <1aslan1ismail@gmail.com>
2022-03-07 03:56:57 +0300

    fix arrayfield

--- server/todo_api/models.py
+++ server/todo_api/models.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.fields import ArrayField
 class Task(models.Model):
     id = models.TextField(blank=True,primary_key=True)
     content = models.TextField(null=True,blank=True)
-    tags = ArrayField( models.TextField(null=True,blank=True))
+    tags = ArrayField(base_field=models.CharField(max_length=200, null=True), default=list, blank=True)
     assignedTo = models.TextField(null=True,blank=True)
     color = models.TextField(null=True,blank=True)
    
@@ -14,7 +14,7 @@ class Task(models.Model):
 class Column(models.Model):
     id = models.TextField(blank=True,primary_key=True)
     title = models.TextField(null=True,blank=True)
-    taskIds = ArrayField( models.TextField(null=True,blank=True))
+    taskIds = ArrayField(base_field=models.CharField(max_length=200, null=True), default=list, blank=True)
     
     def __str__(self):
         return f"{self.id} {self.title}"
